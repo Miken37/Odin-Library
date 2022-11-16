2b0af4643062fcafccbce9e5635329a0049d8dd4
Miken37 <mike-166@hotmail.com>
2022-11-16 18:37:08 -0500

    Adding book functional with prompts. Working on using form instead. Design of card complete.

--- script.js
+++ script.js
@@ -4,11 +4,22 @@ function log(words){
 
 let myLibrary = [];
 
-function addBookToLibrary(){
-    let bookTitle = input("Enter book title: ");
-    let bookAuthor = input("Enter book author: ");
-    let bookPageCount = input("Enter book page count: ");
-    let bookReadStatus = input("Did you read it? y/n: ");
+
+function addBookToLibrary(){ //RE WRITE THIS AS A FORM
+    /*let bookTitle = prompt("Enter book title: ");
+    let bookAuthor = prompt("Enter book author: ");
+    let bookPageCount = prompt("Enter book page count: ");
+    let bookReadStatus = prompt("Did you read it? y/n: ");*/
+    /*const book = new Book(bookTitle, bookAuthor, bookPageCount, bookReadStatus);
+    myLibrary.push(book);*/
+    log(myLibrary);
+    const form = document.querySelector(".formContainer");
+    form.style.display = "none";
+    if (form.style.display === "none"){
+        form.style.display = "flex";
+    } else {
+        form.style.display = "none";
+    }
 }
 
 function Book(title, author, pages, readStatus){
@@ -18,18 +29,50 @@ function Book(title, author, pages, readStatus){
     this.readStatus = readStatus
 }
 
-
-const book1 = new Book('The Hobbit', 'JRR Tolkien', 300, "Y");
-myLibrary.push(book1);
-
-const book2 = new Book('The Hobbit', 'JRR Tolkien', 300, "Y");
-myLibrary.push(book2);
-
-const book3 = new Book('The Hobbit', 'JRR Tolkien', 300, "Y");
-myLibrary.push(book3);
-
-const book4 = new Book('The Hobbit', 'JRR Tolkien', 300, "Y");
-myLibrary.push(book4);
+const cardContainer = document.querySelector(".card-container");
+const card = document.querySelector(".card");
 
 
-log(myLibrary);
+function bookCard(){
+    const cardElement = document.createElement("div")
+    cardElement.classList.add('card');
+    cardContainer.appendChild(cardElement);
+
+    const cardTitleElement = document.createElement("h1");
+    cardTitleElement.classList.add('card-title');
+    cardElement.appendChild(cardTitleElement);
+    cardTitleElement.innerText = myLibrary[myLibrary.length-1].title;       //Referencing index: OF LENGTH -1
+
+    const cardAuthorElement = document.createElement("p");
+    cardAuthorElement.classList.add('card-author');
+    cardElement.appendChild(cardAuthorElement);
+    cardAuthorElement.innerText = "Written by: " + myLibrary[myLibrary.length-1].author;
+
+    const cardPagesElement = document.createElement("p");
+    cardPagesElement.classList.add('card-pages');
+    cardElement.appendChild(cardPagesElement);
+    cardPagesElement.innerText = "Pages: " + myLibrary[myLibrary.length-1].pages;
+
+    const cardReadElement = document.createElement("p");
+    cardReadElement.classList.add('card-read');
+    cardElement.appendChild(cardReadElement);
+    cardReadElement.innerText = "Read: " + myLibrary[myLibrary.length-1].readStatus;
+}
+
+/*Old bookCard function with forEach loop
+function bookCard(){
+    myLibrary.forEach(book => {
+        const test = document.createElement("div")
+        myLibrary.pop();        //Removes item from array so it is able to add only ONE item at a time instead of the entire array. 
+        test.classList.add('card');
+        cardContainer.appendChild(test);
+    });
+}
+*/
+
+
+const addBookButton = document.querySelector(".add-book");
+addBookButton.addEventListener("click", () => {
+    addBookToLibrary();
+    bookCard();
+})
